98fe9dcf92be8f30bff0f01cefd22f31a504a8e0
SagiBeno <benosagi321@gmail.com>
2025-09-17 08:05:49 +0200

    Show error page

--- react-app/src/backend/express.cjs
+++ react-app/src/backend/express.cjs
@@ -1,7 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'error.html')) // __dirname --> OP lekérdezés
+})
+
 const port = 3333
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Express backend server is running on port', port)
 })
